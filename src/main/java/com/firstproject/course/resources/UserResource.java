3eb8bc89cee8024c1e88b329c1cde084255d327d
regys-mendes <regyseduardomendes123@gmail.com>
2026-04-30 23:16:57 -0300

    User entity and resource

---
+++ src/main/java/com/firstproject/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.firstproject.course.resources;
+
+import com.firstproject.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+       User u = new User(1L, "Bob", "bob@gmail.com", "319995739", "123445");
+       return ResponseEntity.ok().body(u);
+    }
+
+
+
+}
