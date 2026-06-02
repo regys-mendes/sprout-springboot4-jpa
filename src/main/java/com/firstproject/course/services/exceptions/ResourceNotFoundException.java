0ddbb584a7f048e03c7b87d085855ea785c60b59
regys-mendes <regyseduardomendes123@gmail.com>
2026-06-02 19:26:19 -0300

    Exception handling - findById

---
+++ src/main/java/com/firstproject/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.firstproject.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
