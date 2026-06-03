3afcf1d92e59b83499991d24e30919d4d4a02862
regys-mendes <regyseduardomendes123@gmail.com>
2026-06-03 19:23:30 -0300

    Exception handling - delete

--- src/main/java/com/firstproject/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/firstproject/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.firstproject.course.resources.exceptions;
 
+import com.firstproject.course.services.exceptions.DatabaseException;
 import com.firstproject.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> dataBase(@NonNull DatabaseException e, HttpServletRequest request){
+        String error = "Data base error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/firstproject/course/services/UserService.java
+++ src/main/java/com/firstproject/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.firstproject.course.services;
 
 import com.firstproject.course.repositories.UserRepository;
 import com.firstproject.course.entities.User;
+import com.firstproject.course.services.exceptions.DatabaseException;
 import com.firstproject.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -41,7 +44,16 @@ public class UserService {
     }
 
     public void delete(Long id){
-         repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
+
     
 }
